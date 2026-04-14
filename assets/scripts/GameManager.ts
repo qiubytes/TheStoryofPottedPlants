3be1ee92be2cd0732050100a1d756318bdfeb702
yuhandbcontext <yuhandbcontext@noreply.gitcode.com>
2026-04-14 21:25:55 +0800

    获取数据 更新scrollview

--- assets/scripts/GameManager.ts
+++ assets/scripts/GameManager.ts
@@ -1,5 +1,5 @@
 import { _decorator, Component, Game, Label, Node } from 'cc';
-import { DataManager, PlantLevel } from './DataManager';
+import { DataManager, Plant, PlantLevel } from './DataManager';
 const { ccclass, property } = _decorator;
 
 @ccclass('GameManager')
@@ -10,6 +10,9 @@ export class GameManager extends Component {
     //收益速度
     @property(Label)
     speedValueLabel: Label;
+    //植物ScrollView
+    @property(Node)
+    plantScrollView: Node;
     //单例引用
     public static inst: GameManager;
 
@@ -52,9 +55,30 @@ export class GameManager extends Component {
             }
             //速度
             this.speedValueLabel.string = "+" + speed.toString() + "/s";
-
+            //更新scrollView里面的icon数据
+            this.updatePlantScrollView();
             //重置计时器
             this.dataTimer = 0;
+        }
+    }
+    //根据数据更新 scrollView里面的植物的Icon等级、收益速度
+    private updatePlantScrollView() {
+        let content: Node = this.plantScrollView.getChildByPath("view/content");
+        let gameData = DataManager.inst.gameData;
+        let platLevelDef = DataManager.inst.gamePlatLevelDef;
+        // console.log("获取conent节点");
+        // console.log(content);
+        let plantIcons: Node[] = content.children;
+        for (let i = 0; i < plantIcons.length; i++) {
+            let name: string = plantIcons[i].getChildByPath("NameLabel").getComponent(Label).string;
+            let plant: Plant = gameData.plants.find(t => t.name == name);
+            if (plant) {
+                plantIcons[i].getChildByPath("LevelLabel").getComponent(Label).string = "Lv." + plant.level;
+                plantIcons[i].getChildByPath("LevelLabel").getComponent(Label).string = "Lv." + plant.level;
+                let plantLevel = platLevelDef.find(t => t.name == name);
+                let speed = plant.level * plantLevel.baseHpPerSec;
+                plantIcons[i].getChildByPath("SpeedLabel").getComponent(Label).string = speed + "/s";
+            }
         }
     }
 }
